ac0cf6087ac07271c38e83819de1e1b21a7f7df9
ujwala <132659209+ujwala1720@users.noreply.github.com>
2023-12-16 21:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,38 +7,35 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
 
-app = Flask(__name__)
-
-# Set OpenAI API key
-os.environ["OPENAI_API_KEY"] = "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"
-# Your URLs for document loading
-urls = ["https://coal.nic.in/acts-rules-policies"]
-loaders = UnstructuredURLLoader(urls)
-data = loaders.load()
-text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200)
-docs = text_splitter.split_documents(data)
-embeddings = OpenAIEmbeddings()
-vectorstore_OpenAI = FAISS.from_documents(docs, embeddings)
-vectorstore_OpenAI.save_local("faiss_store")
-
-# Load FAISS store and OpenAI LLM
-kal = FAISS.load_local("faiss_store", OpenAIEmbeddings())
-llm = OpenAI(temperature=0, model_name='text-davinci-003')
-chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=kal.as_retriever())
-
+app = Flask(_name_)
 
 @app.route('/')
 def index():
-    return render_template('minbot23.html')
+    return render_template('main_page.html')
+@app.route('/main')
+def second():
+    return render_template('botinterface.html')
 
+@app.route('/process_input', methods=['POST'])
+def process_input():
+    z = request.json.get('user_input', '')
 
-@app.route('/ask_question', methods=['POST'])
-def ask_question():
-    if request.method == 'POST':
-        question = request.json['question']
-        answer = chain({"question": question}, return_only_outputs=True)
-        return jsonify({'answer': answer})
+    os.environ["OPENAI_API_KEY"] = "paste api key"
+    urls = ["https://coal.nic.in", "https://mines.gov.in"]
+    loaders = UnstructuredURLLoader(urls)
+    data = loaders.load()
+    text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200)
+    docs = text_splitter.split_documents(data)
+    embeddings = OpenAIEmbeddings()
+    vectorstore_OpenAI = FAISS.from_documents(docs, embeddings)
+    vectorstore_OpenAI.save_local("faiss_store")
+    kal = FAISS.load_local("faiss_store", OpenAIEmbeddings())
+    llm = OpenAI(temperature=0, model_name='text-davinci-003')
+    chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=kal.as_retriever())
+    ans = chain({"question": z}, return_only_outputs=True)
+    response = ans['answer']
+    return jsonify({'response': response})
 
-
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True)
+
